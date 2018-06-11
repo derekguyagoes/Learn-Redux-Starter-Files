f32f86b08b2a193bdc25701733b49f384d752f5e
derekg <derekg@prioritydispatch.net>
2018-06-11 11:41:34 -0600

    End of video 13

---
+++ learn-redux/client/components/Comments.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+const Comments = React.createClass({
+  render() {
+    return <div className="comment">i'm the comments</div>;
+  }
+});
+
+export default Comments;
